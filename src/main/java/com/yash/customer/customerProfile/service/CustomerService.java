32515d0f5fb188427aa8e16af882f3e8f769c563
mittalyash394 <mittalyash394@gmail.com>
2023-10-01 23:57:31 +0530

    registration and getAllCustomers are done

--- src/main/java/com/yash/customer/customerProfile/service/CustomerService.java
+++ src/main/java/com/yash/customer/customerProfile/service/CustomerService.java
@@ -3,6 +3,10 @@ package com.yash.customer.customerProfile.service;
 import com.yash.customer.customerProfile.dto.RegisterCustomerDto;
 import com.yash.customer.customerProfile.entity.CustomerProfile;
 
+import java.util.List;
+
 public interface CustomerService {
     CustomerProfile registerCustomer(RegisterCustomerDto registerCustomerDto);
+
+    List<CustomerProfile> getAllCustomers();
 }
